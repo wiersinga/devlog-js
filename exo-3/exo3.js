3fb762b179eae37e3e4ecaa2bf181a737c252eee
olfa wiersinga <wiersingaolfa@yahoo.fr>
2023-05-23 17:02:05 +0200

    ok

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -3,25 +3,22 @@ console.log('exo-3');
 
 //console.log(playerInput.toLowerCase());
 const choices = ['rock','paper','scissors'];
-isJuste(choices);
-var playerInput = prompt ("your choice (rock, paper, scissors) ?");
-/*
-function isJuste (choices){
-for (let i = 0 ; i < choices.length ; i++ ) {
 
-    if (playerInput == choices[i]){
-        console.log(playerInput.toLowerCase());
-    } else {
-        console.log("erreur, essayer de nouveauy")
+var playerInput = prompt ("your choice (rock, paper, scissors) ?");
+function getPlayerChoice (PlayerInput){
+    playerInput = playerInput.toLowerCase;
+    for (let i=0; i< choices.length; i++){
+        if (playerInput == choices[i]){
+            console.log(playerInput);
+        } else {
+            
+            console.log();
+        }
     }
 }
-}*/
 
-
-
-let getPlayerChoice = getRandomChoice(choices);
-console.log(getPlayerChoice);
-
+let getComputerChoice = getRandomChoice(choices);
+console.log(getComputerChoice);
 function getRandomChoice(choices){
      const randomIndex= Math.floor(Math.random() * choices.length) ;
      const index = choices[randomIndex];
